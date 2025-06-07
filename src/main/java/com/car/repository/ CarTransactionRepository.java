277069c5bdf3bc3bb00bf201c029ab34f831b0e3
Ruslan-fil <real.madrid4123@gmail.com>
2025-06-07 15:32:15 +0300

    Виправив назву CarTransactionRepository

--- src/main/java/com/car/repository/ CarTransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.car.repository;
-
-import com.car.entity.CarTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarTransactionRepository extends JpaRepository<CarTransaction, Long> {
-}
