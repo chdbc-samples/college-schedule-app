9446fe2ad1d0e6f6e73894927bd4c6f4655d0a1c
Dmytro Nochevnov <dmitry.ndp@gmail.com>
2025-12-12 13:17:24 +0200

    Set UTF-8 encoding when write to database
    Change character encoding to UTF-8 for CSV reading.

--- src/main/java/com/college/CollegeApplication.java
+++ src/main/java/com/college/CollegeApplication.java
@@ -77,7 +77,8 @@ public class CollegeApplication implements CommandLineRunner {
     }
 
     private void addScheduleFromCsv() {
-        try (CSVReader reader = new CSVReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("schedule.csv")))) {
+        try (CSVReader reader = new CSVReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("schedule.csv"),
+        StandardCharsets.UTF_8))) {
             List<String[]> records = reader.readAll();
             
             records.remove(0); // Видалити перший рядок з назвами стовпців
